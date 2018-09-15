f1664a38e5eb6fca2c0840fc2c4712213c3b295c
trumat <matru@protonmail.com>
2018-09-15 15:07:58 +0100

    Exercise 6.2

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.user;
+
+public class SimpleUser {
+    String username;
+
+    public SimpleUser(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
