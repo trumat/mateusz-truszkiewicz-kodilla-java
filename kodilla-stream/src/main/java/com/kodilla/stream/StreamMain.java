bf3db6c5559fc90916b1336ee4538d5bb93517c3
trumat <matru@protonmail.com>
2018-09-26 20:05:03 +0200

    Exercise 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,7 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -27,5 +28,8 @@ public class StreamMain {
         beautifier.beautify(poem, (text) -> text.toLowerCase());
         beautifier.beautify(poem, (text) -> "~*~**" + text + "**~*~");
         beautifier.beautify(poem, (text) -> text.replace(' ', '_'));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
