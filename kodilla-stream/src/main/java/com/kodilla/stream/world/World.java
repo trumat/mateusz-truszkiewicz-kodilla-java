97fddb66040eba21811eaffc01bc98a073a2b4db
trumat <matru@protonmail.com>
2018-10-01 22:33:07 +0100

    Exercise 7.4

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
@@ -0,0 +1,20 @@
+package com.kodilla.stream.world;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class World {
+    private final List<Continent> continents = new ArrayList<>();
+
+    public BigDecimal getPeopleQuantity(){
+        return continents.stream()
+                .flatMap(continent -> continent.getCountries().stream())
+                .map(country -> country.getPeopleQuantity())
+                .reduce(BigDecimal.ZERO, (sum, current) -> sum.add(current));
+    }
+
+    public void addContinent(Continent continent){
+        continents.add(continent);
+    }
+}
---
+++ kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
@@ -0,0 +1,44 @@
+package com.kodilla.stream.world;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorldTestSuite {
+    @Test
+    public void testGetPeopleQuantity(){
+        //Given
+        Country poland = new Country(new BigDecimal("38088141"));
+        Country ireland = new Country(new BigDecimal("4814483"));
+        Country macedonia = new Country(new BigDecimal("2085531"));
+        Continent europe = new Continent();
+        europe.addCountry(poland);
+        europe.addCountry(ireland);
+        europe.addCountry(macedonia);
+        Country congo = new Country(new BigDecimal("5435771"));
+        Country liberia = new Country(new BigDecimal("4884857"));
+        Country egypt = new Country(new BigDecimal("99843445"));
+        Continent africa = new Continent();
+        africa.addCountry(congo);
+        africa.addCountry(liberia);
+        africa.addCountry(egypt);
+        Country nicaragua = new Country(new BigDecimal("6301912"));
+        Country brazil = new Country(new BigDecimal("211270554"));
+        Country ecuador = new Country(new BigDecimal("16924484"));
+        Continent southAmerica = new Continent();
+        southAmerica.addCountry(nicaragua);
+        southAmerica.addCountry(brazil);
+        southAmerica.addCountry(ecuador);
+        World world = new World();
+        world.addContinent(europe);
+        world.addContinent(africa);
+        world.addContinent(southAmerica);
+        //When
+        BigDecimal worldPopulation = world.getPeopleQuantity();
+        //Then
+        Assert.assertEquals(new BigDecimal("389649178"), worldPopulation);
+    }
+}
