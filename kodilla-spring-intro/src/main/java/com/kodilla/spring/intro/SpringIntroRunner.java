daf6136d8b2dd81e3d520177881011a44c46bd88
trumat <matru@protonmail.com>
2018-10-12 16:14:22 +0100

    Exercise 10.1

---
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/SpringIntroRunner.java
@@ -0,0 +1,7 @@
+package com.kodilla.spring.intro;
+
+public class SpringIntroRunner {
+    public static void main (String[] args) {
+        System.out.println("Welcome to the intro to Spring");
+    }
+}
